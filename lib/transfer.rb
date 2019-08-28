bd400286c89b0f27a88138b97f67b6f5295af05d
Angie Goza <angielg9888@gmail.com>
2019-08-28 19:17:27 +0000

    Automatically backed up by Learn

--- lib/transfer.rb
+++ lib/transfer.rb
@@ -37,9 +37,11 @@ attr_accessor :status
   end
   
   def reverse_transfer
-    sender.balance += amount
-    receiver.balance -= amount
-    self.status = "reversed"
+    if self.status = "complete"
+      sender.balance += amount
+      receiver.balance -= amount
+      self.status = "reversed"
+    end
   end
   
       
